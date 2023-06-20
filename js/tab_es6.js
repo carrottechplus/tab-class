5386c7482d45b12cdbe3c9cd8bc930aa4c81ea42
carrottechplus <blohzzang@gmail.com>
2023-06-20 12:15:59 +0900

    고도화

--- js/tab_es6.js
+++ js/tab_es6.js
@@ -1,9 +1,16 @@
 class Tab {
 	constructor(selector, option) {
-		this.tab = document.querySelector(selector);
-		this.btns = this.tab.querySelectorAll(option.btns);
-		this.boxs = this.tab.querySelectorAll(option.boxs);
+		// defaults, result는 바뀌는 값이 아니기 때문에 굳이 인스턴스 객체에 전달할 필요 없음 >> 지역변수로 설정함.
+		const defaults = { btns: 'ul li', boxs: 'article div' };
+		const result = { ...defaults, ...option };
 
+		this.tab = document.querySelector(selector);
+		this.btns = this.tab.querySelectorAll(result.btns);
+		this.boxs = this.tab.querySelectorAll(result.boxs);
+
+		this.bindingEvent();
+	}
+	bindingEvent() {
 		this.btns.forEach((el, idx) => {
 			el.addEventListener('click', () => {
 				[this.btns, this.boxs].forEach((el) => this.activation(el, idx));
@@ -16,7 +23,5 @@ class Tab {
 		arr[idx].classList.add('on');
 	}
 }
-new Tab('#tab', {
-	btns: 'ul li',
-	boxs: 'article div',
-});
+
+// new Tab('#tab'); index.html에 script defer 제거 후 플러그인 호출하듯이 html파일로 이동
