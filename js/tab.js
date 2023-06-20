3aaec23a11cd54e13a454c9e6c22b39e795ee092
carrottechplus <blohzzang@gmail.com>
2023-06-20 12:02:31 +0900

    es6, 객체지향으로 변경

--- js/tab.js
+++ js/tab.js
@@ -1,21 +1,34 @@
-// es5
+//객체지향
 
-var tab = document.querySelector('#tab');
-var btns = tab.querySelectorAll('ul li');
-var boxs = tab.querySelectorAll('article div');
-
-btns.forEach(function (btn, idx) {
-	btn.addEventListener('click', function () {
-		// 버튼 활성화
-		btns.forEach(function (btn) {
-			btn.classList.remove('on');
-		});
-		btns[idx].classList.add('on');
-
-		// 박스 활성화
-		boxs.forEach(function (box) {
-			box.classList.remove('on');
-		});
-		boxs[idx].classList.add('on');
-	});
+new Tab('#tab', {
+	btns: 'ul li',
+	boxs: 'article div',
 });
+
+function Tab(selector, option) {
+	this.tab = document.querySelector(selector);
+	this.btns = this.tab.querySelectorAll(option.btns);
+	this.boxs = this.tab.querySelectorAll(option.boxs);
+
+	this.btns.forEach(
+		function (btn, idx) {
+			btn.addEventListener(
+				'click',
+				function () {
+					[this.btns, this.boxs].forEach(
+						function (el) {
+							this.activation(el, idx);
+						}.bind(this)
+					);
+				}.bind(this)
+			);
+		}.bind(this)
+	);
+}
+// 함수를 프로토타입에 저장
+Tab.prototype.activation = function (arr, idx) {
+	arr.forEach(function (el) {
+		el.classList.remove('on');
+	});
+	arr[idx].classList.add('on');
+};
